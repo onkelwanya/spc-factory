b89b1d4fbb80d06050cbb5d41598ba751dad62af
Jan <jan.svitkin@googlemail.com>
2024-09-17 23:18:26 +0200

    adjusted packaging and prepared for add. release

--- webpack.common.cjs
+++ webpack.common.cjs
@@ -1,5 +1,7 @@
 // eslint-disable-next-line @typescript-eslint/no-require-imports
 const path = require('path');
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     entry: './src/module.ts',
@@ -13,10 +15,21 @@ module.exports = {
                 test: /\.ts$/,
                 use: 'ts-loader',
                 exclude: /node_modules/
+            },
+            {
+                test: /\.hbs$/,
+                use: 'raw-loader',
             }
         ]
     },
     resolve: {
         extensions: ['.ts', '.js']
-    }
+    },
+    plugins: [
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: 'src/templates', to: 'templates' }
+            ]
+        })
+    ]
 };
